b126c980223d0b2886b902dd01de6357a28f89f3
Rodrigo Miguel <rodrigosmig@gmail.com>
2023-10-22 21:05:44 -0300

    Primeiro commit

---
+++ src/main/java/br/com/tech/challenge/sistemapedido/SistemaPedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech.challenge.sistemapedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tech/challenge/sistemapedido/SistemaPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech.challenge.sistemapedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
