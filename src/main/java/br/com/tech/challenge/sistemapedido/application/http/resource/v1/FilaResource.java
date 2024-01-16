7b94096d15d2e7c89f0df3c17219a4684834b76b
Rodrigo Miguel <rodrigosmig@gmail.com>
2024-01-16 20:01:59 -0300

    refactor: cria a classe FilaController

--- src/main/java/br/com/tech/challenge/sistemapedido/application/http/resource/v1/FilaResource.java
+++ src/main/java/br/com/tech/challenge/sistemapedido/application/http/resource/v1/FilaResource.java
@@ -1,8 +1,7 @@
 package br.com.tech.challenge.sistemapedido.application.http.resource.v1;
 
+import br.com.tech.challenge.sistemapedido.application.controller.FilaController;
 import br.com.tech.challenge.sistemapedido.application.response.ListarPedidosResponse;
-import br.com.tech.challenge.sistemapedido.application.mapper.PedidoDataMapper;
-import br.com.tech.challenge.sistemapedido.usecase.contract.pedido.BuscarPedidoUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,21 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/pedidos/fila")
 public class FilaResource {
-    private final BuscarPedidoUseCase buscarPedidoUseCase;
-    private final PedidoDataMapper pedidoMapper;
+    private final FilaController controller;
 
     @GetMapping("restaurante")
     public ResponseEntity<ListarPedidosResponse> filaRestaurante() {
-        var pedidos = buscarPedidoUseCase.buscarFilaRestaurante();
-        var resposta = new ListarPedidosResponse(pedidoMapper.toList(pedidos));
+        var resposta = controller.filaRestaurante();
 
         return new ResponseEntity<>(resposta, HttpStatus.OK);
     }
 
     @GetMapping("cliente")
     public ResponseEntity<ListarPedidosResponse> filaCLiente() {
-        var pedidos = buscarPedidoUseCase.buscarFilaCliente();
-        var resposta = new ListarPedidosResponse(pedidoMapper.toList(pedidos));
+        var resposta = controller.filaRestaurante();
 
         return new ResponseEntity<>(resposta, HttpStatus.OK);
     }
