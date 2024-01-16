7a6a0b897ee9dea7ed970fada1bd70f9ae6d69ed
Rodrigo Miguel <rodrigosmig@gmail.com>
2024-01-16 19:55:47 -0300

    refactor: cria a classe PedidoController

--- src/main/java/br/com/tech/challenge/sistemapedido/application/http/resource/v1/PedidoResource.java
+++ src/main/java/br/com/tech/challenge/sistemapedido/application/http/resource/v1/PedidoResource.java
@@ -1,14 +1,9 @@
 package br.com.tech.challenge.sistemapedido.application.http.resource.v1;
 
+import br.com.tech.challenge.sistemapedido.application.controller.PedidoController;
 import br.com.tech.challenge.sistemapedido.application.request.PedidoRequest;
 import br.com.tech.challenge.sistemapedido.application.response.CadastrarPedidoResponse;
 import br.com.tech.challenge.sistemapedido.application.response.ListarPedidosResponse;
-import br.com.tech.challenge.sistemapedido.application.mapper.ItemPedidoDataMapper;
-import br.com.tech.challenge.sistemapedido.application.mapper.PedidoDataMapper;
-import br.com.tech.challenge.sistemapedido.usecase.contract.pedido.AlterarStatusPedidoUseCase;
-import br.com.tech.challenge.sistemapedido.usecase.contract.pedido.BuscarPedidoUseCase;
-import br.com.tech.challenge.sistemapedido.usecase.contract.pedido.CriarPedidoUseCase;
-import br.com.tech.challenge.sistemapedido.usecase.contract.pedido.PagarPedidoUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,54 +13,47 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/pedidos")
 public class PedidoResource {
-    private final CriarPedidoUseCase criarPedidoUseCase;
-    private final BuscarPedidoUseCase buscarPedidoUseCase;
-    private final PagarPedidoUseCase pagarPedidoUseCase;
-    private final AlterarStatusPedidoUseCase alterarStatusPedidoUseCase;
-
-    private final ItemPedidoDataMapper itemPedidoMapper;
-    private final PedidoDataMapper pedidoMapper;
+    //adicionar o swagger
+    private final PedidoController controller;
 
     @PostMapping
     public ResponseEntity<CadastrarPedidoResponse> criar(@RequestBody PedidoRequest request) {
-        var pedido = criarPedidoUseCase.criar(itemPedidoMapper.toDomainList(request.itens()));
-        var resposta = new CadastrarPedidoResponse(pedido.getId());
+        var resposta = controller.criar(request);
 
         return new ResponseEntity<>(resposta, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<ListarPedidosResponse> listar() {
-        var pedidos = buscarPedidoUseCase.buscarTodos();
-        var resposta = new ListarPedidosResponse(pedidoMapper.toList(pedidos));
+        var resposta = controller.listar();
 
         return new ResponseEntity<>(resposta, HttpStatus.CREATED);
     }
 
     @PostMapping("{idPedido}/pagar")
     public ResponseEntity<Void> pagar(@PathVariable Long idPedido) {
-        pagarPedidoUseCase.pagar(idPedido);
+        controller.pagar(idPedido);
 
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("preparacao/{idPedido}")
     public ResponseEntity<Void> preparacao(@PathVariable Long idPedido) {
-        alterarStatusPedidoUseCase.alterarParaEmPreparacao(idPedido);
+        controller.preparacao(idPedido);
 
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("pronto/{idPedido}")
     public ResponseEntity<Void> pronto(@PathVariable Long idPedido) {
-        alterarStatusPedidoUseCase.alterarParaPronto(idPedido);
+        controller.pronto(idPedido);
 
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("finalizado/{idPedido}")
     public ResponseEntity<Void> finalizado(@PathVariable Long idPedido) {
-        alterarStatusPedidoUseCase.alterarParaFinalizado(idPedido);
+        controller.finalizado(idPedido);
 
         return ResponseEntity.noContent().build();
     }
