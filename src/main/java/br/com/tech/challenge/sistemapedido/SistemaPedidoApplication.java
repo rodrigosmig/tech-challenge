25c3533658cc6d8e1456d88e1f865f4198e604e6
Rodrigo Miguel <rodrigosmig@gmail.com>
2024-01-21 10:50:19 -0300

    chore: adiciona o openFeign ao projeto

--- src/main/java/br/com/tech/challenge/sistemapedido/SistemaPedidoApplication.java
+++ src/main/java/br/com/tech/challenge/sistemapedido/SistemaPedidoApplication.java
@@ -2,7 +2,9 @@ package br.com.tech.challenge.sistemapedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SistemaPedidoApplication {
 
